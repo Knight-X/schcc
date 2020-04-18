d6f3dd47cb77f156383e7508580c943ef7205112
kazami <kazamizero@gmail.com>
2020-04-18 13:59:29 +0800

    start

---
+++ main.sh
@@ -0,0 +1,45 @@
+#lang racket
+(define env0 '())
+
+
+(define ext-env
+  (lambda (x v env)
+    (cons `(,x . ,v) env)))
+
+(define lookup
+  (lambda (x env)
+    (let ([p (assq x env)])
+      (cond
+        [(not p) #f]
+        [else (cdr p)]))))
+
+(struct Closure (f env))
+
+(define interp
+  (lambda (exp env)
+    (match exp
+      [(? number? x) x]
+      [(? symbol? x) (let ([v (lookup x env)])
+                       (cond
+                         [(not v)
+                          (error "undefined variable" x)]
+                         [else v]))]
+      [`(let ([,x ,e1]) ,e2)
+       (let ([v1 (interp e1 env)])
+         (interp e2 (ext-env x v1 env)))]
+      [`(lambda (,x) ,e)
+       (Closure exp env)]
+      [`(,e1 ,e2)
+       (let ([v1 (interp e1 env)]
+             [v2 (interp e2 env)])
+         (match v1
+           [(Closure `(lambda (,x) ,e) env-save)
+            (interp e (ext-env x v2 env-save))]))]
+      [`(,op ,e1 ,e2)
+       (let ([v1 (interp e1 env)]
+             [v2 (interp e2 env)])
+         (match op
+           ['+ (+ v1 v2)]
+           ['- (- v1 v2)]
+           ['* (* v1 v2)]
+           ['/ (/ v1 v2)]))])))
